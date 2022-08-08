25c01e11a2a761bd688ec66d331071093537c2b6
Alexis Asseman <alexis@semiotic.ai>
2022-08-08 13:26:57 -0700

    Fix TestSharedSubgraph query volume test + formatting

--- tests/price_multiplier_bandit/test_shared_subgraph.py
+++ tests/price_multiplier_bandit/test_shared_subgraph.py
@@ -1,20 +1,30 @@
 # Copyright 2022-, Semiotic AI, Inc.
 # SPDX-License-Identifier: Apache-2.0
 
-import pytest
-import random
-from price_multiplier_bandit.shared_subgraph import NoisySharedSubgraph
 from asyncio import run
-import numpy as np
 from typing import Sequence
+
+import numpy as np
+import pytest
+
+from price_multiplier_bandit.shared_subgraph import NoisySharedSubgraph
+
 
 class TestSharedSubgraph:
     @pytest.mark.unit
     @pytest.mark.parametrize("number_of_agents", [1, 2, 3, 4])
-    @pytest.mark.parametrize("cost_multipliers", [[1e-6, 1e-6, 1e-6, 1e-6], [1.1e-6, 1.2e-6, 1.3e-6, 1.4e-6], [1e-7, 5e-5, 1e-9, 3e-6]])
-    def test_sum_queries_close_multipliers(self, number_of_agents: int, cost_multipliers: Sequence[float]):
-        
-        env = NoisySharedSubgraph()
+    @pytest.mark.parametrize(
+        "cost_multipliers",
+        [
+            [1e-7, 1e-7, 1e-7, 1e-7],
+            [1.1e-7, 1.2e-7, 1.3e-7, 1.4e-7],
+            [1e-7, 5e-5, 1e-9, 3e-6],
+        ],
+    )
+    def test_query_volume(
+        self, number_of_agents: int, cost_multipliers: Sequence[float]
+    ):
+        env = NoisySharedSubgraph(cost_multiplier_threshold=1e-6)
 
         # Run the environment for 3 steps
         for _ in range(3):
