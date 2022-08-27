3dd7e7bc270290e75cc2490d3796091237ff64d7
Alexis Asseman <alexis@semiotic.ai>
2022-08-26 19:43:28 -0700

    multi_agent_simultation: PyQtGraph instead of Matplotlib

--- simulation/multi_agent_simulation.py
+++ simulation/multi_agent_simulation.py
@@ -4,11 +4,11 @@
 import argparse
 import logging
 from asyncio import run
-from re import A
 
-import matplotlib.animation as animation
-import matplotlib.pyplot as plt
-from matplotlib import colors
+import ffmpeg
+import numpy as np
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtCore, QtGui, QtWidgets
 
 from environments.simulated_subgraph import SimulatedSubgraph
 from simulation.controller import init_simulation
@@ -16,6 +16,8 @@ from simulation.show_bandit import add_experiment_argparse
 
 logging.basicConfig(level="WARN", format="%(message)s")
 
+
+def main():
     # Init argparse.
     parser = argparse.ArgumentParser(
         usage="%(prog)s [-c ...] [-i ...] [--show] [--save]",
@@ -28,23 +30,69 @@ logging.basicConfig(level="WARN", format="%(message)s")
     # We need the environment to be SimulatedSubgraph
     assert isinstance(environment, SimulatedSubgraph)
 
-    # Generate the filename.
-    FILENAME = f"{args.config}.mp4"
-
-    fig, ax = plt.subplots()
+    ffmpeg_process = None
 
     # Environment x.
     min_x = 1e-10
     max_x = 5e-6
 
-    # Create containers.
-    image_container = []
-    legend_container = []
-    agent_colors = [
-        colors.to_rgba(c) for c in plt.rcParams["axes.prop_cycle"].by_key()["color"]
-    ]
+    # Set up PyQtGraph
+    pg.setConfigOption("background", "white")
+    pg.setConfigOption("foreground", "black")
+    pg.setConfigOptions(antialias=True)
+    app = pg.mkQApp("Plot")
+    win = pg.GraphicsLayoutWidget(show=not args.save, title="Multi-agent training")
+    win.resize(1000, 800)
 
-    print(f"Training {len(agents)} x agents on {environment}. Please wait...")
+    # Create policy plot
+    plot_1 = win.addPlot(title="time 0")
+    plot_1.setPreferredHeight(700)
+    plot_1.addLegend(offset=(-1, 1))
+    plot_1.setYRange(0, 1.3)
+    plot_1.setLabel("left", "Queries/s")
+    plot_1.setLabel("bottom", "Price multiplier")
+    # Policy PD
+    agents_dist = [
+        plot_1.plot(
+            pen=pg.mkPen(color=(i, len(agents)), width=2),
+            name=f"Agent {agent_name}: policy",
+        )
+        for i, agent_name in enumerate(agents.keys())
+    ]
+    # Initial policy PD
+    agents_init_dist = [
+        plot_1.plot(
+            pen=pg.mkPen(color=(i, len(agents)), width=2, style=QtCore.Qt.DotLine),  # type: ignore
+            name=f"Agent {agent_name}: init policy",
+        )
+        for i, agent_name in enumerate(agents.keys())
+    ]
+    # Environment QPS
+    env_plot = plot_1.plot(
+        pen=pg.mkPen(color="gray", width=2), name="Environment: total q/s"
+    )
+    agents_scatter_qps = pg.ScatterPlotItem()
+    plot_1.addItem(agents_scatter_qps)
+
+    win.nextRow()
+
+    # Create query volume bar graph
+    plot_2 = win.addPlot()
+    plot_2.setPreferredHeight(300)
+    plot_2.setXRange(0, 1.5)
+    plot_2.setLabel("left", "Agent #")
+    plot_2.setLabel("bottom", "Queries/s")
+    y = [i for i in range(len(agents))]
+    w = [0 for _ in range(len(agents))]
+    qps_bars = pg.BarGraphItem(
+        x0=0,
+        y=y,
+        height=0.7,
+        width=w,
+        brushes=[(i, len(agents)) for i in range(len(agents))],
+    )
+    plot_2.addItem(qps_bars)
+
     for i in range(args.iterations):
         logging.debug("=" * 20 + " step %s " + "=" * 20, i)
 
@@ -52,11 +100,7 @@ logging.basicConfig(level="WARN", format="%(message)s")
         if i % args.fast_forward_factor == 0:
             # Plot environment.
             env_x, env_y = run(environment.generate_plot_data(min_x, max_x))
-            (im_env,) = plt.plot(env_x, env_y, color="grey")
-
-            # Add new list to image container.
-            image_container.append([im_env])
-            legend_container.append(["Environment: total q/s"])
+            env_plot.setData(env_x, env_y)
 
         # Execute actions for all agents.
         scaled_bids = []
@@ -126,74 +170,72 @@ logging.basicConfig(level="WARN", format="%(message)s")
 
         # X. Collect the values for visualization of agent's gaussian policy.
         if i % args.fast_forward_factor == 0:
+            agents_scatter_qps.clear()
+
             for agent_id, (agent_name, agent) in enumerate(agents.items()):
 
                 # Get data.
                 data = run(agent.generate_plot_data(min_x, max_x))
                 agent_x = data.pop("x")
                 agent_y = data["policy"]
-                agent_color = agent_colors[agent_id % len(agent_colors)]
+                agents_dist[agent_id].setData(agent_x, agent_y)
 
-                # Plot policy and add it to last list in container.
-                (img_agent,) = plt.plot(agent_x, agent_y, color=agent_color)
-                image_container[-1].append(img_agent)
-                legend_container[-1].append(f"Agent {agent_name}: policy")
+                qps_bars.setOpts(width=queries_per_second)
 
                 # Plot init policy and add it to last list in container.
                 if "init policy" in data.keys():
                     init_agent_y = data["init policy"]
-                    (img_init_agent,) = plt.plot(
-                        agent_x, init_agent_y, color=agent_color, linestyle="dashed"
-                    )
-                    image_container[-1].append(img_init_agent)
-                    legend_container[-1].append(f"Agent {agent_name}: init policy")
+                    agents_init_dist[agent_id].setData(agent_x, init_agent_y)
 
                 # Agent q/s.
                 agent_qps_x = min(max_x, max(min_x, scaled_bids[agent_id]))
-                img_agent_qps = plt.scatter(
-                    [agent_qps_x],
-                    [queries_per_second[agent_id]],
-                    marker="o",
-                    color=agent_color,
+                agents_scatter_qps.addPoints(
+                    [
+                        {
+                            "pos": (agent_qps_x, queries_per_second[agent_id]),
+                            "brush": (agent_id, len(agents)),
+                            "pen": pg.mkPen(color=(agent_id, len(agents)), width=1),
+                        }
+                    ]
                 )
-                image_container[-1].append(img_agent_qps)
-                legend_container[-1].append(f"Agent {agent_name}: action => q/s")
 
-            # Generate labels & title.
-            ax.set_xlabel("Price multiplier")
-            # ax.set_ylabel('queries/s ')
-            title = ax.text(
-                0.5,
-                1.05,
-                f"time {i}",
-                size=plt.rcParams["axes.titlesize"],
-                ha="center",
-                transform=ax.transAxes,
-            )
-
-            # Create legend object.
-            legend = ax.legend(image_container[-1], legend_container[-1])  # type: ignore
-
-            # Add title to last list in container.
-            image_container[-1].append(title)  # type: ignore
+            plot_1.setTitle(f"time {i}")
 
         # 5. Make a step.
         environment.step()
-
-    if args.show or args.save:
-        print("Plotting animation...")
-        ani = animation.ArtistAnimation(
-            fig, image_container, interval=50, blit=False, repeat_delay=1000
-        )
-
-        # Set up formatting for the movie files.
-        Writer = animation.writers["ffmpeg"]
-        writer = Writer(fps=15, metadata=dict(artist="Me"), bitrate=1800)
-
-        if args.show:
-            plt.show()
+        QtWidgets.QApplication.processEvents()  # type: ignore
 
         if args.save:
-            print(f"Saving movie, please wait...")
-            ani.save(FILENAME, writer=writer)
-            print(f"Movie saved to '{FILENAME}'")
+            if i % args.fast_forward_factor == 0:
+                if not ffmpeg_process:
+                    # Start ffmpeg to save video
+                    FILENAME = f"{args.config}.mp4"
+                    ffmpeg_process = (
+                        ffmpeg.input(
+                            "pipe:", format="rawvideo", pix_fmt="rgb24", s="1000x800"
+                        )
+                        .output(FILENAME, vcodec="libx264", pix_fmt="yuv420p")
+                        .overwrite_output()
+                        .run_async(pipe_stdin=True)
+                    )
+
+                qimage = win.grab().toImage()
+                qimage = qimage.convertToFormat(
+                    QtGui.QImage.Format_RGB888, QtCore.Qt.AutoColor  # type: ignore
+                )
+
+                ffmpeg_process.stdin.write(qimage.constBits().tobytes())
+
+        else:  # Show
+            if win.isHidden():
+                break
+
+    if ffmpeg_process:
+        ffmpeg_process.stdin.close()
+        ffmpeg_process.wait()
+
+    pg.exit()
+
+
+if __name__ == "__main__":
+    main()
